489fd80a0b981710e47cdbc7cce6facd56959567
jmaydev-personal <jmaydev@gmail.com>
2026-03-05 21:17:50 +1100

    tweak vite config for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Set base to your GitHub repo name, e.g. '/langloop/'
+// If deploying to a custom domain or username.github.io root repo, use '/'
 export default defineConfig({
   plugins: [react()],
+  base: '/langloop/',
 })
